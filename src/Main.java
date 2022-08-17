a9e22c847621db6e96999c08889f3216b69f599e
Устюгов <alexust11@yandex.ru>
2022-08-17 12:59:15 +0700

    поправил переменные и применил рекомендуемое форматирование

--- src/Main.java
+++ src/Main.java
@@ -14,27 +14,33 @@ public class Main {
         //System.out.println("Hello world!");
 
         // Задача 1
-        int Summa = 0;
-        for (int i = 0; i < arr.length; i++) {
-            Summa = Summa + arr[i];
+        int summa = 0;
+        for (int j : arr) {
+            summa = summa + j;
         }
-        System.out.println("Сумма трат за месяц составила " + Summa + " рублей");
+        System.out.println("Сумма трат за месяц составила " + summa + " рублей");
         // Задача 2
-        int WasteMin = arr[0];
-        int WasteMax = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i]>WasteMax) {WasteMax=arr[i];}
-            if (arr[i]<WasteMin) {WasteMin=arr[i];}
-                }
-             System.out.println("Минимальная сумма трат за день составила "+WasteMin+" рублей. Максимальная сумма трат за день составила "+WasteMax+" рублей");
+        int wasteMin = arr[0];
+        int wasteMax = arr[0];
+        for (int j : arr) {
+            if (j > wasteMax) {
+                wasteMax = j;
+            }
+            if (j < wasteMin) {
+                wasteMin = j;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + wasteMin + " рублей. Максимальная сумма трат за день составила " + wasteMax + " рублей");
         // Задача 3
-             float sred=Summa/30;
-            System.out.println("Средняя сумма трат за месяц составила "+sred+" рублей");
+        var sred = summa / 30;
+        System.out.println("Средняя сумма трат за месяц составила " + sred + " рублей");
         // Задача 4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i=reverseFullName.length-1; i>=0; i--)  {System.out.print(reverseFullName[i]);}
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
+}
 
 
 
